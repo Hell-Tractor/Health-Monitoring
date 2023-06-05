28ad5caadb5e0512860e8777e0c13c83cfdd7a86
吴佳熙 <1006961384@qq.com>
2023-06-05 15:13:04 +0800

    尝试修改访问后端api转发失败的问题

--- client/src/api/data.js
+++ client/src/api/data.js
@@ -1,6 +1,6 @@
 import {get} from '@/utils/request'
 export default {
-    getHeartRate:()=>get('/data_url/data?dataType=heartRate&page=1&pageSize=1'),
+    getHeartRate:()=>get('/data?dataType=heartRate&page=1&pageSize=1'),
     getBloodPressureLow:()=>get('/data_url/data?dataType=bloodPressureLow&page=1&pageSize=1'),
     getBloodPressureHigh:()=>get('/data_url/data?dataType=bloodPressureHigh&page=1&pageSize=1'),
     getBloodOxygen:()=>get('/data_url/data?dataType=bloodOxygen&page=1&pageSize=1'),
