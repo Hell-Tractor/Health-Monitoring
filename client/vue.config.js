98837908e87a8cdd37ea52d5f169c3002c27e4b5
吴佳熙 <1006961384@qq.com>
2023-06-05 14:03:15 +0800

    修改转发路径

--- client/vue.config.js
+++ client/vue.config.js
@@ -14,47 +14,7 @@ module.exports = {
         pathRewrite: {
           "^/data_url": ''
         }
-      },
-      '/staff_url': {
-        //target: 'http://192.168.202.1:8082', // 后端接口地址
-        target: 'http://localhost:8082',
-        changeOrigin: true,
-        pathRewrite: {
-          "^/staff_url": ''
-        }
-      },
-      '/rule_url': {
-        //target: 'http://192.168.202.1:8083', // 后端接口地址
-        target: 'http://localhost:8083',
-        changeOrigin: true,
-        pathRewrite: {
-          "^/rule_url": ''
-        }
-      },
-      '/forecast_url': {
-        //target: 'http://192.168.202.1:8084', // 后端接口地址
-        target: 'http://localhost:8084',
-        changeOrigin: true,
-        pathRewrite: {
-          "^/forecast_url": ''
-        }
-      },
-      '/preference_url':{
-        //target: 'http://192.168.202.1:8082',
-        target: 'http://localhost:8082',
-        changeOrigin: true,
-        pathRewrite: {
-          "^/preference_url": ''
-        }
-      },
-      '/schedule_url':{
-        //target: 'http://192.168.202.1:8085',
-        target: 'http://localhost:8085',
-        changeOrigin: true,
-        pathRewrite: {
-          "^/schedule_url": ''
-        }
-      },
+      }
     }
   },
 };
