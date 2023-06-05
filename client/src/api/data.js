a2dc52e6957960d23ad2598a58a8941e39bc9bb2
吴佳熙 <1006961384@qq.com>
2023-06-05 15:17:45 +0800

    修改后端api转发

--- client/src/api/data.js
+++ client/src/api/data.js
@@ -1,14 +1,14 @@
 import {get} from '@/utils/request'
 export default {
     getHeartRate:()=>get('/data?dataType=heartRate&page=1&pageSize=1'),
-    getBloodPressureLow:()=>get('/data_url/data?dataType=bloodPressureLow&page=1&pageSize=1'),
-    getBloodPressureHigh:()=>get('/data_url/data?dataType=bloodPressureHigh&page=1&pageSize=1'),
-    getBloodOxygen:()=>get('/data_url/data?dataType=bloodOxygen&page=1&pageSize=1'),
-    getFloorNumByPeriod:()=>get('/data_url/data/summary/sum/interval/4?dataType=floorClimbed'),
-    getStepNumByPeriod:()=>get('/data_url/data/summary/sum/interval/4?dataType=stepCount'),
-    getWalkDistanceByPeriod:()=>get('/data_url/data/summary/sum/interval/4?dataType=walkingDistance'),
-    getRunDistanceByPeriod:()=>get('/data_url/data/summary/sum/interval/4?dataType=runningDistance'),
-    getHeartRateWarn:()=>get('/data_url/data/warn?dataType=heartRate'),
-    getBloodPressureWarn:()=>get('/data_url/data/warn?dataType=bloodPressureHigh'),
-    getBloodOxygenWarn:()=>get('/data_url/data/warn?dataType=bloodOxygen')
+    getBloodPressureLow:()=>get('/data?dataType=bloodPressureLow&page=1&pageSize=1'),
+    getBloodPressureHigh:()=>get('/data?dataType=bloodPressureHigh&page=1&pageSize=1'),
+    getBloodOxygen:()=>get('/data?dataType=bloodOxygen&page=1&pageSize=1'),
+    getFloorNumByPeriod:()=>get('/data/summary/sum/interval/4?dataType=floorClimbed'),
+    getStepNumByPeriod:()=>get('/data/summary/sum/interval/4?dataType=stepCount'),
+    getWalkDistanceByPeriod:()=>get('/data/summary/sum/interval/4?dataType=walkingDistance'),
+    getRunDistanceByPeriod:()=>get('/data/summary/sum/interval/4?dataType=runningDistance'),
+    getHeartRateWarn:()=>get('/data/warn?dataType=heartRate'),
+    getBloodPressureWarn:()=>get('/data/warn?dataType=bloodPressureHigh'),
+    getBloodOxygenWarn:()=>get('/data/warn?dataType=bloodOxygen')
 }
