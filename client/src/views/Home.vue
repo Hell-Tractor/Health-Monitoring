7db2e448f711b6d29ba2703a5da3aaa99749ebde
吴佳熙 <1006961384@qq.com>
2023-06-09 23:27:45 +0800

    修改配色

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -312,7 +312,7 @@ export default {
             type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
           }
         },
-        color: ['#fac858', '#ee6666'],
+        color: ['#ee6666','#fac858'],
         legend: {},
         grid: {
           left: '3%',
