d73a2b9d566c0f90047777328837ab12fe5bee22
Hell-Tractor <1104840448@qq.com>
2023-12-29 18:58:52 +0800

    swap api.rs and static.rs, update docker scripts

--- server/scripts/create-service.sh
+++ server/scripts/create-service.sh
@@ -1,7 +1,7 @@
 echo "Creating docker images for health-monitoring-server"
-docker build -t health-monitoring-server-api -f Dockerfile.api .
-docker build -t health-monitoring-server-static -f Dockerfile.static .
+docker build -t health-monitoring-server-api-docker -f Dockerfile.api .
+docker build -t health-monitoring-server-static-docker -f Dockerfile.static .
 
 echo "running docker images"
-docker run -d -p 9999:9999 health-monitoring-server-api -e MYSQL_PASSWORD=$MYSQL_PASSWORD
-docker run -d -p 9998:9998 health-monitoring-server-static
+docker run -d -p 9999:9999 -e MYSQL_PASSWORD=$MYSQL_PASSWORD health-monitoring-server-api-docker
+docker run -d -p 9998:9998 health-monitoring-server-static-docker
--- server/src/bin/api.rs
+++ server/src/bin/api.rs
@@ -1,13 +1,20 @@
-use actix_web::{HttpServer, App};
+use actix_web::{HttpServer, App, web};
 use health_monitoring_server::common::SETTINGS;
+use health_monitoring_server::data;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = SETTINGS.read().unwrap().get("server.port").unwrap();
     println!("Server started at http://0.0.0.0:{port}");
     HttpServer::new(|| {
-        App::new().service(actix_files::Files::new("", "./static").index_file("index.html"))
-        .wrap(actix_web::middleware::Logger::default())
+        App::new().service(
+            web::scope("/data")
+                .service(data::controller::insert_data)
+                .service(data::controller::retrieve_data)
+                .service(data::controller::retrieve_data_summary)
+                .service(data::controller::get_data_group_by_hour)
+                .service(data::controller::get_data_warn)
+        ).wrap(actix_web::middleware::Logger::default())
     }).bind(("0.0.0.0", port))?
     .run()
     .await
